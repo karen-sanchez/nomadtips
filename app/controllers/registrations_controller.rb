2cb9c1174f75c5cfbeaa3655da5a0a710516a1f4
Karen Sanchez <karensanchez@Christophers-MacBook-Pro-3.local>
2017-03-07 16:34:25 -0500

    profiles

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,9 +2,9 @@ class RegistrationsController < Devise::RegistrationsController
 
   private
 
-  # def sign_up_params
-  #   params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
-  # end
+  def sign_up_params
+    params.require(:user).permit(:email, :username, :password, :password_confirmation)
+  end
 
   def account_update_params
     params.require(:user).permit(:email, :username, :nationality, :about, :thumbnail, :password, :password_confirmation, :current_password)
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
 
+  get 'users/index'
+
   devise_for :users, :controllers => { registrations: 'registrations' }
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "application#index"
+
+  # match '/users',   to: 'users#index',   via: 'get'
+
+  match '/users/:id',     to: 'users#show',       via: 'get'
+
+  # devise_for :users, :path_prefix => 'd'
+  # resources :users, :only =>[:show]
   
 end
