150952ef52476f44cec7722d6abeee0f8709ca54
Karen Sanchez <karensanchez@Christophers-MacBook-Pro-3.local>
2017-03-09 17:11:18 -0500

    posts by user displayed

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: "application#index"
+  root to: "application#main"
 
   resources :posts, only: [:index, :edit, :new, :create, :destroy]
 
