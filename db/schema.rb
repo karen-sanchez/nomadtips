64aa90cfa54fd907b97b77ee48efac7e00ef2a2b
Karen Sanchez <karensanchez@ChristohersMBP3.home>
2017-04-22 10:54:24 -0400

    added columns to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170309183355) do
+ActiveRecord::Schema.define(version: 20170422144735) do
 
   create_table "posts", force: :cascade do |t|
     t.integer  "user_id"
@@ -46,6 +46,8 @@ ActiveRecord::Schema.define(version: 20170309183355) do
     t.string   "thumbnail_content_type"
     t.integer  "thumbnail_file_size"
     t.datetime "thumbnail_updated_at"
+    t.string   "first_name"
+    t.string   "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
